4490af781c0f153aa90970c4771ca6c04e787be2
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-08-15 23:31:45 +0300

    add site

---
+++ script.js
@@ -0,0 +1,17 @@
+
+"use strict";
+
+let input1 = document.getElementById("inp1");
+let input2 = document.getElementById("inp2");
+let calculateButton = document.getElementById("calcbtn");
+let resultInput = document.getElementById("resultInp");
+
+calculateButton.addEventListener('click', function() {
+    let sum = "";
+    sum = `${input1.value}${input2.value}`;
+    resultInput.value = sum;
+    if(input1.value == "" && input2.value == "") {
+        sum = 0;
+        resultInput.value = sum;
+    }
+});
