2174e2e9e5fd2028f3ab8ae9897110a6e5a8d7c4
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-10-23 14:49:55 +0300

    Fix script

--- script.js
+++ script.js
@@ -5,17 +5,13 @@ let input1 = document.getElementById("inp1");
 let input2 = document.getElementById("inp2");
 let calculateButton = document.getElementById("calcbtn");
 let resultInput = document.getElementById("resultInp");
-let title = document.getElementById("title");
 
 calculateButton.addEventListener('click', function() {
     let sum = "";
     sum = `${input1.value}${input2.value}`;
     resultInput.value = sum;
-    title.style.color = 'white';
-    title.style.textTransform = 'capitalize';
-    title.innerText = 'Typical JS Calculator';
     if(input1.value == "" && input2.value == "") {
         sum = 0;
         resultInput.value = sum;
     }
-});
+})
