97a41bc001dfb6888fe56aa3cf2b6dff4a13e2b2
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-05-12 11:15:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,20 @@ let resultInput = document.getElementById("resultInp");
 
 calculateButton.addEventListener('click', function() {
     let sum = "";
-    sum = `${input1.value}${input2.value}`;
-    resultInput.value = sum;
     if(input1.value == "" && input2.value == "") {
         sum = 0;
+        resultInput.value = sum;
+    }
+    else if(input1.value != "" && input2.value == "") {
+        sum = `${input1.value}0`;
+        resultInput.value = sum;
+    }
+    else if(input1.value == "" && input2.value != "") {
+        sum = `0${input2.value}`;
+        resultInput.value = sum;
+    }
+    else {
+        sum = `${input1.value}${input2.value}`;
         resultInput.value = sum;
     }
 })
